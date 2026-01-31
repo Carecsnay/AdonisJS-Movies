88caccf5b38ed670266d5bd0db155c2f37a69279
Bruno Desktop <carecsnay1@gmail.com>
2026-01-31 00:11:09 +0000

    ✨ (categorias_controller): add index method to return sample data ✨ (routes): add route for categorias and import CategoriasController

--- app/controllers/categorias_controller.ts
+++ app/controllers/categorias_controller.ts
@@ -1,4 +1,7 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
 export default class CategoriasController {
+  index() {
+    return [1, 2, 3]
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,12 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const CategoriasController = () => import('#controllers/categorias_controller')
 
 router.get('/', async () => {
   return {
-    hello: 'world',
+    hello: 'AdonisJS Movies',
   }
 })
+
+router.get('/categorias', [CategoriasController, 'index'])
